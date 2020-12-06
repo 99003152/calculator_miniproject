ea5535953f32ec932864c08b8885d0ac2978b0a1
Aiswarya Sasikumar <69103516+stepin104959@users.noreply.github.com>
2020-12-06 12:31:38 +0530

    Area Commit

--- implementation/calci.h
+++ implementation/calci.h
@@ -14,6 +14,15 @@ double vol_cuboid(double x, double y, double z);
 double vol_sphere(double x, double y, double z);
 double vol_cone(double x, double y, double z);
 double vol_cylinder(double x, double y, double z);
+double Scalene();
+double Isosceles();
+double Equilateral();
+double B_H_Triangle();
+double Circle();
+double Rectangle();
+double Square();
+double Trapezium();
+
 
 
 #endif
