d59c3bb91582f7d922cc30c2cd734906d59db67e
99003151 <75420464+99003151@users.noreply.github.com>
2020-12-06 18:39:41 +0530

    Create Calculator_header.h

---
+++ Abacus_Calculator/Implementation/inc/Calculator_header.h
@@ -0,0 +1,26 @@
+#define _CALCI_H_
+#define _CALCI_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <stdio_ext.h>
+
+int add(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+int div(int x, int y);
+int squr(int x);
+int vol_cube(int x, int y, int z);
+int vol_cuboid(int x, int y, int z);
+int vol_sphere(int x, int y, int z);
+int vol_cone(int x, int y, int z);
+int vol_cylinder(int x, int y, int z);
+int Triangle(int x,int y);
+int Circle(int x);
+int Rectangle(int x,int y);
+int Square(int x);
+
+
+
+
+#endif
